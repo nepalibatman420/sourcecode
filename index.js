3e94b44452b8ea49a1f1b3bd444fb09e1a19496a
nepalibatman420 <121029789+nepalibatman420@users.noreply.github.com>
2022-12-19 17:45:50 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const element = React.createElement('h1'  , { className: 'w3-xxlarge' } , 'Hello, World!');
-const element1 = React.createElement('div' , { className: 'w3-card' }, 'Sup yo!');
+const element1 = React.createElement('div' , { className: 'w3-card' }, 'Sup yo222222222222!');
     ReactDOM.render(element, document.getElementById('root'));
     ReactDOM.render(element1, document.getElementById('root1'));
 
